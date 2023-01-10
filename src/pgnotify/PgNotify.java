17ffdeb1f8238ff8c6ac71a3e4ecb38f3801f222
Ivan Jesus Castro Ruiz <119132185+IvanCastroRuizdevitech@users.noreply.github.com>
2023-01-10 11:36:15 -0500

    Update PgNotify.java

--- src/pgnotify/PgNotify.java
+++ src/pgnotify/PgNotify.java
@@ -22,8 +22,8 @@ public class PgNotify {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         // TODO code application logic here
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://ws.desarrollo.eds.terpel.sclbox.com:12015/andromeda";
-        Connection conn = DriverManager.getConnection(url,"pegaso","NEdweV1qJCZ4Zj1Lc0tKLVVEQXt3flk7ICY+Z19lfkVgcjw2L05VeW0tMm4neyVTPlo9clV2TjR0Xns0");
+        String url = "jdbc:postgresql://ip/BD";
+        Connection conn = DriverManager.getConnection(url,"pegaso","clave");
         //Listener listener =  new Listener(conn).start();
     }
     
